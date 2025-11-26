cce4b9d84f06210eea4bccd1239240e23d8cbe95
LouzaliAbdelhamid <samuiun@live.fr>
2025-11-26 21:31:32 +0100

    Move pages to /pages folder and fix switch_page paths

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import streamlit as st
+from components import header
 
 st.set_page_config(page_title="Mini-Kahoot", layout="centered")
 
-st.title("🎉 Mini-Kahoot")
+header("🎉 Mini-Kahoot")
 
 st.write("Choisissez votre mode :")
 
-col1, col2 = st.columns(2)
+if st.button("🧑‍🏫 Mode Prof"):
+    st.switch_page("prof")
 
-with col1:
-    if st.button("👨‍🏫 Mode Prof"):
-        st.switch_page("prof.py")
-
-with col2:
-    if st.button("🎓 Mode Élève"):
-        st.switch_page("eleve.py")
+if st.button("🎓 Mode Élève"):
+    st.switch_page("eleve")
